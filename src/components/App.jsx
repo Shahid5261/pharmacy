43c4850cd1d4b91d0062344f7854989e3cc8476d
Shahid5261 <90671128+Shahid5261@users.noreply.github.com>
2024-03-30 14:18:01 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+//import Bucket from "./Bucket";
+import Image from "./Image"
+
+function App(){
+
+    return <div>
+        <Navbar/>
+       
+        <Footer/>
+        <Image/>
+         
+        
+       
+        
+    </div>
+}
+export default App;
---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.png";
+
+
+function Image ()
+{
+
+    return <div className="Image">
+        <img src={logo} alt="logo" />
+    </div>
+}
+export default Image;
